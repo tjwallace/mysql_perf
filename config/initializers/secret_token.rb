b52302d8ef42895e915fc6f047ee49f413d0f284
Jeff Wallace <jeff@tjwallace.ca>
2011-09-05 19:02:04 -0700

    blank project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MysqlPerf::Application.config.secret_token = '8c32dfba43bef825af7efd985ce7de19401642f6f994dfe5e9a36411742fa8c0099eedbe200ee1f7687e031acd04833a1ecaf25aea6273a6decd2be6fda85df5'
---
+++ config/initializers/time_ext.rb
@@ -0,0 +1,8 @@
+class Time
+  def self.random(years_back = 2)
+    year = Time.now.year - rand(years_back) - 1
+    month = rand(12) + 1
+    day = rand(31) + 1
+    Time.local(year, month, day)
+  end
+end
